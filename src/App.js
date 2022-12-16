87829f83822ec9bcaa3337a0bbc96437c26e9b80
fuadhasan08 <fuadhasandipro08@gmail.com>
2022-12-16 23:25:07 +0600

    'basic'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className='center'>
+      <div className='content'>
+        <h1>Bacic React Setup</h1>
+        <p>Start Editing From Here.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
